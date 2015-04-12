8e329718215a317cda052fc98131bebcac2eb723
Linhn <lhainan09@163.com>
2015-04-12 00:49:59 -0700

    Lhainan: get the article page from website

--- tools/MyHTMLParser.py
+++ tools/MyHTMLParser.py
@@ -26,3 +26,28 @@ class MyHTMLParser(HTMLParser):
                 if value == "ImgSearch":
                     print("data"+data)
 
+class XQHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.links = []
+        self.attrs = []
+ 
+    def handle_starttag(self, tag, attrs):
+        #print "Encountered the beginning of a %s tag" % tag
+        if tag == "div":
+            if len(attrs) == 0: pass
+            else:
+                #print(attrs)
+                self.attrs = attrs
+                for (variable, value)  in attrs:
+                    if variable == "class":
+                        if value == "ImgSearch":
+                            print('find')
+                #return attrs
+    def handle_data(self, data):
+        print(data)
+        attrs = self.attrs
+        for (variable, value)  in attrs:
+            if variable == "class":
+                if value == "ImgSearch":
+                    print("data"+data)
